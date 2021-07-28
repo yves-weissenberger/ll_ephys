871407e2598d09abd432704e5190a5ded693b074
Yves Weissenberger <yves@Yvess-MacBook-Pro.local>
2021-07-28 20:35:16 +0100

    update

--- packages/mecll/scripts/extract_position.py
+++ packages/mecll/scripts/extract_position.py
@@ -28,7 +28,7 @@ if __name__== "__main__":
 
     unprocessed_videos = []
     for vf in video_files:
-        print(vf)
+        #print(vf)
         if 'OF' in vf:
             big_arena = 'OFB' in vf  #adjust sizes of everything. Also need to do a calibration
             
@@ -37,6 +37,7 @@ if __name__== "__main__":
 
                 if any([(f_root + res_type) not in processed_data for res_type in res_extensions]):
                     video_path = os.path.join(video_dir,vf)
+                    print(video_path)
                     position = extract_position_from_video(video_path)
                     occ_map = split_occupancy_map(position)
                     np.save(os.path.join(res_folder,vf+'_positions.npy'),position)

03d5b73e1fd0405f784c36465b73ddb16d1804c3
Yves Weissenberger <yves@Yvess-MacBook-Pro.local>
2021-10-04 14:30:50 +0100

    added tests + restructuring

--- packages/mecll/GLM.py
+++ packages/mecll/GLM.py
@@ -1,7 +1,10 @@
 import numpy as np
 from scipy.special import factorial
 
-def _pdf(rate,n):
+#These are relevant things for estimating poinsson
+
+
+def _pmf(rate,n):
     return (rate**n)*np.exp(-rate)/factorial(n)
 
 def _logL(rate,n):
@@ -9,20 +12,21 @@ def _logL(rate,n):
 
 
 def logL(beta,x,y):
-    rate = np.exp(np.dot(beta,x))
+    """ returns the negavtive log-likelihood """
+    rate = np.exp(np.dot(x,beta))
     LL = np.sum(y*np.log(rate)-rate)
     return -LL
 
 def grad(beta,x,y):
     
-    g = np.exp(np.dot(beta,x))
-    J = -x.dot(y-g)
+    g = np.exp(np.dot(x,beta))
+    J = -x.T.dot(y-g)
     return J
 
 def hess(beta,x,y):
 
-    g = np.exp(np.dot(beta,x))
-    return -(np.dot(x*-g,x.T))
+    g = np.exp(np.dot(x,beta))
+    return -(np.dot(x.T*-g,x))
 
 
 
---
+++ packages/test/test_example.py
@@ -0,0 +1,9 @@
+import unittest
+from mecll import GLM
+
+class TestModule(unittest.TestCase):
+    def test_module(self):
+        self.assertEqual(1,1)
+
+if __name__ == '__main__':
+    unittest.main()

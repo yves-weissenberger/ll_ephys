f95550ffc254d8f754140189f0e22675f0bfcd06
Yves Weissenberger <yves@Yvess-MacBook-Pro.local>
2021-10-07 12:24:38 +0100

    new analysis

--- packages/mecll/scripts/TB_analysis.py
+++ packages/mecll/scripts/TB_analysis.py
@@ -12,9 +12,32 @@ Thus
 
 
 def collect_transitions():
-
+    
 
 
 if __name__=="__main__":
 
-    U,S,Vt = np.linalg.SVD(mean_activity)
+    n_states = 9
+    for session in sessions:
+        mean_activity = get_mean_activity()  #get the mean activity during each poke
+
+        collect_single_trials = n_states = 9
+        U,S,Vt = np.linalg.SVD(mean_activity)  
+
+        #take the first n_states columns of U
+        U_short = U[:,:n_states]
+
+        trial_matrix = get_single_trial_matrix()  #T.shape = (n_cells x n_timepoints)
+        
+        #now get the expression of each of the 9 singular vectors in the population activity???
+        #each row of y is a mixture of the rows of trial_matrix. This is just horizontally stacked trials.
+        #each column of y is a mixture of the columns of U.T. What are the columns of U.T
+        y = np.dot(U_short.T,trial_matrix)
+
+
+
+
+
+
+
+

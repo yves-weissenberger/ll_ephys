1d8641bb75ef89f50db86e037a8aa98ff498c8f0
Yves Weissenberger <yvesweissenberger@gmail.com>
2022-02-16 17:11:29 +0000

    installed

--- packages/mecll/setup.py
+++ packages/mecll/setup.py
@@ -1,12 +1,4 @@
-import os
 from setuptools import setup
-
-
-
-def post_install():
-    data_directory = input("Specify a path for data to be stored: ")
-    input("Specify a path for user info to be stored: ")
-    input("Provide name of email address for use: ")
     
 
 
